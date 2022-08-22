63b95d83a6c47e14d20cbc5d8fec0e5e2208e910
alfonharyos <109057552+alfonharyos@users.noreply.github.com>
2022-08-22 19:27:52 +0700

    first app upload

---
+++ app.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import streamlit as st
+import xlsxwriter
+from filter_emr.filter import preprocess as pp
+from io import BytesIO
+from st_aggrid import AgGrid
+
+def up_file(uploaded_file):
+    with st.spinner('Wait for it...'):
+        if uploaded_file.name[-3:] == 'csv':
+            df = pd.read_csv(uploaded_file)
+            typ = 'csv'
+        elif (uploaded_file.name[-3:] == 'xls') or (uploaded_file.name[-4:] == 'xlsx'):
+            df = pd.read_excel(uploaded_file) 
+            typ = 'xlsx'
+        df = df.astype(str)
+    return df, typ
+
+def update_counter():
+        st.session_state.keluhan = st.session_state.col
+        st.session_state.param_sakit = st.session_state.sakit
+        st.session_state.param_awal = st.session_state.awal
+        st.session_state.param_akhir = st.session_state.akhir
+        st.session_state.param_neg = st.session_state.neg
+
+def convert_df(df, type):
+    if type == 'csv':
+        df = df.to_csv().encode('utf-8')
+    elif (type == 'xls') or (type == 'xlsx'):
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df.to_excel(writer, index=False) 
+        writer.save() 
+        df = output.getvalue()
+    return df
+
+
+st.title(' EMR Preprocessing ')
+st.text('Ekstrasi gejala keluhan pasien')
+
+uploaded_file = st.file_uploader('Choose EMR file', 
+                                type=['csv','xlsx', 'xls'],
+                                accept_multiple_files=False)
+
+if uploaded_file != None:   
+
+    if 'df_up' not in st.session_state:
+        st.session_state.df_up, st.session_state.data_type = up_file(uploaded_file)
+
+    st.header('EMR Display')
+    if 'extract_gejala' in st.session_state.df_up.columns: 
+        AgGrid(st.session_state.df_up.\
+                drop(['extract_gejala'], axis=1).\
+                reset_index(), theme='streamlit')
+    else: 
+        AgGrid(st.session_state.df_up.\
+                reset_index(), theme='streamlit')
+
+    # preprocessing
+    if 'keluhan' not in st.session_state:
+        st.session_state.keluhan = None
+        st.session_state.param_sakit = 'luka;sakit;nyeri'
+        st.session_state.param_awal = 'diagnosa;keluhan;dengan;riwayat;kontrol'
+        st.session_state.param_akhir = 'sejak;+'
+        st.session_state.param_neg = 'tidak;-'   
+    try:
+        with st.form(key='my_form'):
+            st.selectbox(   label='Pilih column keluhan pasien',
+                            options=[None]+st.session_state.df_up.columns.tolist(),
+                            help='Pilih column yang akan diekstrak',
+                            index=0, key='col')
+            st.write('Parameter (default)')
+            with st.expander("Merubah Nilai Parameter"):
+                st.text_input(  label='Parameter Sakit',
+                                value=st.session_state.param_sakit,
+                                help='Perameter sakit: tanda/kata yang mewakili target/gejala', 
+                                key='sakit')
+                st.text_input(  label='Parameter Awal', 
+                                value=st.session_state.param_awal,
+                                help='Perameter awal: tanda/kata yang letaknya berada sebelum target/gejala', 
+                                key='awal')
+                st.text_input(  label='Parameter Akhir', 
+                                value=st.session_state.param_akhir,
+                                help='Perameter akhir: tanda/kata yang letaknya berada setelah target/gejala', 
+                                key='akhir')
+                st.text_input(  label='Parameter Negatif', 
+                                value=st.session_state.param_neg,
+                                help='tanda/kata yang menandakan bahwa pasien tidak memiliki gejala',
+                                key='neg')
+            submit = st.form_submit_button(label='Extract', on_click=update_counter)
+    except ValueError:
+        st.warning('Pilih Kolom Keluhan')
+
+    # Extrak Gejala
+    if submit:
+        if 'df_pp' not in st.session_state:
+            df_emr = st.session_state.df_up
+            with st.spinner('Wait for it...'):
+                df_emr['extract_gejala'] = df_emr[st.session_state.keluhan].\
+                                                    apply(lambda x: pp().\
+                                                    get_symptoms(str(x),
+                                                    st.session_state.param_sakit,
+                                                    st.session_state.param_awal,
+                                                    st.session_state.param_akhir,
+                                                    st.session_state.param_neg,
+                                                    5))
+                st.session_state.df_pp = df_emr
+                st.session_state.dl_data = convert_df(df_emr, st.session_state.data_type)
+
+    if 'df_pp' in st.session_state: 
+        with st.spinner('Wait for display...'):
+            df_emr_pp = st.session_state.df_pp
+            AgGrid(df_emr_pp[[st.session_state.keluhan,'extract_gejala']].astype(str).reset_index(), theme='streamlit')
+
+        st.download_button(
+            label="Download Data",
+            data=st.session_state.dl_data,
+            file_name='EMR_preprocessing.'+st.session_state.data_type
+        )
+
+else:
+    st.warning('you need to upload a csv or excel file.')
+    
---
+++ filter_emr/filter.py
@@ -0,0 +1,189 @@
+import pandas as pd
+import json
+import re
+
+class preprocess:
+    
+  def low(self,text):
+    text = text.lower()
+    return text
+
+  def del_punc(self,text):
+    text = text.replace("x000d", "")
+    text = re.sub(r'[!"#$%&\'()*/:;<=>?@[\]^_`{|}~]+', "", text)
+    return text
+
+  def del_num(self,text):
+    text = re.sub(r'\b[0-9]+\b\s*', '', text)
+    return text
+  
+  def replace_param(self,text):
+    # replace newline ==> koma
+    text = text.replace("\n",",")
+    # memberi space pada parameter penentuan gejala sebelum melakukan pembagian gejala
+    # pemberian space pada parameter dengan menggunakan metode berbeda karena dg terkandung juga didalam dengan
+    if 'dg' in text or 'riw' in text :
+      txt = []
+      for w in text.split():
+        if w == 'dg': 
+          w = 'dengan'
+        elif w == 'riw':
+          w = 'riwayat'
+        else : w=w
+        txt.append(w)
+      text = ' '.join(txt)
+    text = text.replace("dgn", "dengan")
+    text = text.replace("dengan", ", dengan ")
+    text = text.replace("riwayat", "riwayat ")
+    text = text.replace("sjk", "sejak")
+    text = text.replace("sejak", "sejak ")
+    text = text.replace("diagnosis", "diagnosa ")
+    text = text.replace("mengeluhkan", "keluhan")
+    text = text.replace("mengeluh", "keluhan")
+    text = text.replace("keluhan", "keluhan ")
+    text = text.replace("tdk", "tidak")
+    text = text.replace("tidak", ", tidak ")
+    text = text.replace(' + ', " + dengan ")
+    text = text.replace('+', " +,")
+    text = text.replace('-', " -,")
+    text = text.replace(',', " ,")
+    text = text.replace('.', " .")
+    return text
+
+  def stop_w(self,text,param_s,param_d,param_b,param_n):
+    # stopword Indonesia
+    with open('filter_emr/stopword-id_tala.txt', 'r') as s:                           
+      stopword = [line.strip() for line in s]
+    with open('filter_emr/add_stopword-id.txt', 'r') as add_s:
+      stopword = stopword+[line.strip() for line in add_s]
+      stopword = [word for word in stopword if word not in param_s+param_d+param_b+param_n]
+    # singkatan yg tidak perlu
+    list_chr = ['yg', 'di', 'yg ll', 'dr', 'sda', 'rs']
+    txt = ' '.join([word for word in text.split() if word not in list_chr+stopword])
+    return txt
+
+  def ubah(self,text):
+    
+    singkatan_dict = json.load( open( "filter_emr/singkatan_dict.json" ) ) # open dictionary
+
+    list_acro = list()
+    for key in singkatan_dict.keys():
+      list_acro.append(key)
+
+    txt = []
+    for w in text.split():
+      if (len(w) == 3 or len(w) == 2) and not w.isnumeric():
+        if w in list_acro:
+          w = singkatan_dict.get(w, w)
+        elif w not in list_acro : w=None
+      elif w in list_acro:
+        w = singkatan_dict.get(w, w)
+      else: w=w
+      txt.append(w)
+    txt = ' '.join(list(filter(None, txt)))
+    return txt.lower()
+
+  def split_txt(self,text):
+    text = re.split('[.,]',text)
+    return text
+
+  def gejala(self,text,param_s,param_d,param_b,param_n,n):
+    txt = []
+    for s in text:
+      # Memilah kalimat yang tidak mengandung negasi
+      if any(set(s.split()) & set(param_n)) == False:
+        
+        # Parameter Sakit terletak sebelum gejala
+        if any(set(s.split()) & set(param_s)) == True:
+          # Mencari index parameter yang paling dekat dengan target
+          ind=[]
+          for p in param_s:
+            if p in s.split():
+              indices = [index for index, element in enumerate(s.split()) if element == p]
+              ind.extend(indices)
+          ind=max(ind)
+          # ambil n kata setelah parameter
+          try:
+            x = ind+n
+            s = ' '.join(s.split()[ind:x])
+            # hapus paramter lain yang ada di dalam kalimat
+            for pb in param_b:
+              if pb in s:
+                s = s.split()
+                ind = s.index(pb)
+                # hapus kata" setelah parameter
+                del s[ind:]
+                s = ' '.join(s)
+            txt.append(s)
+          except ValueError:
+            pass
+
+        # Parameter terletak setelah gejala
+        elif any(set(s.split()) & set(param_b)) == True:
+          for p in param_b:
+            if p in s:
+              try:
+                # mencari index dari parameter di dalam kalimat
+                ind = s.split().index(p)
+                # ambil n kata sebelum parameter
+                x = ind-n
+                if x < 0 : x=0
+                s = ' '.join(s.split()[x:ind])
+                # hapus paramter lain yang ada di dalam kalimat
+                for pd in param_d:
+                  if pd in s:
+                    s = s.split()
+                    ind = s.index(pd)
+                    # hapus kata" sebelum parameter
+                    del s[0:ind+1]
+                    s = ' '.join(s)
+                txt.append(s)
+              except ValueError:
+                pass
+
+        # Parameter terletak sebelum gejala
+        elif any(set(s.split()) & set(param_d)) == True:
+          # Mencari index parameter yang paling dekat dengan target
+          ind=[]
+          for p in param_d:
+            if p in s.split():
+              indices = [index for index, element in enumerate(s.split()) if element == p]
+              ind.extend(indices)
+          ind=max(ind)
+          # ambil n kata setelah parameter
+          try:
+            x = ind+n
+            s = ' '.join(s.split()[ind+1:x+1])
+            # hapus paramter lain yang ada di dalam kalimat
+            for pb in param_b:
+              if pb in s:
+                s = s.split()
+                ind = s.index(pb)
+                # hapus kata" setelah parameter
+                del s[ind:]
+                s = ' '.join(s)
+            txt.append(s)
+          except ValueError:
+            pass
+
+          
+
+    txt = list(filter(None, txt))
+    return txt
+
+  def get_symptoms(self, text:str,param_s, param_d, param_b ,param_n,n:int=5):
+    param_s = [w.strip() for w in param_s.split(';')]
+    param_d = [w.strip() for w in param_d.split(';')]
+    param_b = [w.strip() for w in param_b.split(';')]
+    param_n = [w.strip() for w in param_n.split(';')]
+    text = self.low(text)
+    text = self.del_punc(text)
+    text = self.del_num(text)
+    text = self.replace_param(text)
+    text = self.stop_w(text,param_s,param_d,param_b,param_n)
+    text = self.ubah(text)
+    text = self.split_txt(text) 
+    text = self.gejala(text,param_s,param_d,param_b,param_n,n)
+    if text == []:
+      text = None
+    return text 
